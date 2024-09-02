ee76c5904c3adeb8b2af2e1fe43fa5bbf356c4df
jylu lyapunova <jylilapa@yandex.ru>
2024-09-02 19:31:40 +0300

    old project

---
+++ tests/test_masks.py
@@ -0,0 +1,13 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def tests_get_mask_card_number() -> None:
+    """Тест на проверку маскировки номера карты"""
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+
+
+def tests_get_mask_account() -> None:
+    """Тест на проверку маскировки номера счета"""
+    assert get_mask_account("73654108430135874305") == "**4305"
