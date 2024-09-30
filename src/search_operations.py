dbe71ac5a76dc5382ac5160347b1d6d10062722d
jylu lyapunova <jylilapa@yandex.ru>
2024-09-30 15:37:32 +0300

    fixed bugs

--- src/search_operations.py
+++ src/search_operations.py
@@ -10,7 +10,7 @@ def operations_search(transactions: list[dict], search_string: str) -> list[dict
     for transaction in transactions:
         if re.search(pattern, transaction["description"]):
             operations_filter.append(transaction)
-            return operations_filter
+    return operations_filter
 
 
 def count_description(transactions: list[dict], categories: list) -> dict:
@@ -25,3 +25,50 @@ def count_description(transactions: list[dict], categories: list) -> dict:
                 categories_dict.append(category)
     counted_description = Counter(categories_dict)
     return counted_description
+
+
+print(operations_search([
+  {
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  },
+  {
+    "id": 939719570,
+    "state": "EXECUTED",
+    "date": "2018-06-30T02:08:58.425572",
+    "operationAmount": {
+      "amount": "9824.07",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 75106830613657916952",
+    "to": "Счет 11776614605963066702"
+  },
+  {
+    "id": 587085106,
+    "state": "EXECUTED",
+    "date": "2018-03-23T10:45:06.972075",
+    "operationAmount": {
+      "amount": "48223.05",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 41421565395219882431"
+  }], "Перевод организации"))
